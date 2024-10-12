6418df1677cdbca436146a7edfa8636f6d80dd4c
JoaoVictorCRP <joaovictorxl120@gmail.com>
2024-10-11 21:11:26 -0300

    Aula 11.10 | Feat | Simulando Um banco de dados com a Classe Produto

--- src/main/java/com/example/apimemoriapoo/controller/ProdutoController.java
+++ src/main/java/com/example/apimemoriapoo/controller/ProdutoController.java
@@ -1,4 +1,54 @@
 package com.example.apimemoriapoo.controller;
 
+import com.example.apimemoriapoo.model.Produto;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Random;
+
+@RestController
+@RequestMapping("/produto") // Quem responde em /produto é o ProdutoController
 public class ProdutoController {
+    //Simulando banco de dados
+    private static final Map<Long, Produto> produtos = new HashMap<Long, Produto>();
+    static {
+        produtos.put(1L, new Produto(
+            1L,
+            "Macbook Air",
+            "Processador M1 concebido pela Apple para um enorme avanço no desempenho da CPU, GPU e aprendizagem automática",
+            4999.9
+            )
+        );
+        produtos.put(2L, new Produto(
+            2L,
+            "Xbox Series X",
+            "Descubra o Xbox mais rápido e potente de todos os tempos com o Xbox Series X. Aproveite os jogos em 4K em até 120 quadros por segundo nesse console de última geração.",
+            2599.9
+            )
+        );
+        produtos.put(2L, new Produto(
+            3L,
+            "Playstation 5",
+            "Jogos impressionantes: Maravilhe-se com os gráficos incríveis e experimente os recursos do novo PS5.",
+            2999.9
+            )
+        );
+    }
+
+    @GetMapping
+    public Produto fetchRandom(){
+        Random rand = new Random();
+
+        Long n = rand.nextLong(1,3);
+        return produtos.get(n);
+    }
+
+    @GetMapping("/{id}")
+    public Produto findById(@PathVariable Long id) {
+        return produtos.get(id);
+    }
 }
--- src/main/java/com/example/apimemoriapoo/model/Produto.java
+++ src/main/java/com/example/apimemoriapoo/model/Produto.java
@@ -1,5 +1,55 @@
 package com.example.apimemoriapoo.model;
 
 public class Produto {
+    private Long id;
+    private String nome;
+    private String descricao;
+    private Double preco;
 
+    public Produto(Long id, String nome, String descricao, Double preco) {
+        this.id = id;
+        this.nome = nome;
+        this.descricao = descricao;
+        this.preco = preco;
+    }
+
+    public Produto() {}
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(Double preco) {
+        this.preco = preco;
+    }
+
+    @Override
+    public String toString() {
+        return "{ id:" + id + ", nome:" + nome + ", descricao:" + descricao + '}';
+
+    }
 }
